cd093131a1186976623a121e1d200a6da2cad084
Leonard <balint.leonard1@gmail.com>
2024-04-22 22:04:59 +0300

    test

--- src/main/java/tasks/controller/NewEditController.java
+++ src/main/java/tasks/controller/NewEditController.java
@@ -74,12 +74,21 @@ public class NewEditController {
         this.service =service;
         this.dateService =new DateService(service);
     }
-    public void setCurrentTask(Task task){
-        this.currentTask=task;
-        switch (clickedButton.getId()){
-            case  "btnNew" : initNewWindow("New Task");
+//    public void setCurrentTask(Task task){
+//        this.currentTask=task;
+//        switch (clickedButton.getId()) {
+//            case "btnNew" -> initNewWindow("New Task");
+//            case "btnEdit" -> initEditWindow("Edit Task");
+//        }
+//    }
+    public void setCurrentTask(Task task) {
+        this.currentTask = task;
+        switch (clickedButton.getId()) {
+            case "btnNew":
+                initNewWindow("New Task");
                 break;
-            case "btnEdit" : initEditWindow("Edit Task");
+            case "btnEdit":
+                initEditWindow("Edit Task");
                 break;
         }
     }
@@ -104,13 +113,13 @@ public class NewEditController {
     private void initEditWindow(String title){
         currentStage.setTitle(title);
         fieldTitle.setText(currentTask.getTitle());
-        datePickerStart.setValue(DateService.getLocalDateValueFromDate(currentTask.getStartTime()));
+        datePickerStart.setValue(dateService.getLocalDateValueFromDate(currentTask.getStartTime()));
         txtFieldTimeStart.setText(dateService.getTimeOfTheDayFromDate(currentTask.getStartTime()));
 
         if (currentTask.isRepeated()){
             checkBoxRepeated.setSelected(true);
             hideRepeatedTaskModule(false);
-            datePickerEnd.setValue(DateService.getLocalDateValueFromDate(currentTask.getEndTime()));
+            datePickerEnd.setValue(dateService.getLocalDateValueFromDate(currentTask.getEndTime()));
             fieldInterval.setText(service.getIntervalInHours(currentTask));
             txtFieldTimeEnd.setText(dateService.getTimeOfTheDayFromDate(currentTask.getEndTime()));
         }
